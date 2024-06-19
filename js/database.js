04d74d5e9ae95c60194d8668625bff8544f87df1
Negrão, IPR <neurocientista@gmail.com>
2024-06-18 21:37:47 -0300

    .

--- js/database.js
+++ js/database.js
@@ -75,3 +75,24 @@ const initSlider = () => {
 }
 
 window.addEventListener("load", initSlider);
+
+
+let btnMenu = document.getElementById("btn-menu");
+
+let btnAbrir = document.getElementById("btn-abrir");
+
+let menu = document.getElementById("menu-mobile");
+
+let overlay = document.getElementById("overlay-menu");
+
+btnMenu.addEventListener("click", () => {
+  menu.classList.add("abrir-menu");
+});
+
+menu.addEventListener("click", () => {
+  menu.classList.remove("abrir-menu");
+});
+
+overlay.addEventListener("click", () => {
+  menu.classList.remove("abrir-menu");
+});
